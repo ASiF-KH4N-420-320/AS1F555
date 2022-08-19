af0a2ed4b9d6a3879b5cfccda8780ef5bfd6a348
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2022-08-19 15:49:32 +0500

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    name='AS1F555',
-    version='0.0.0',
-    py_modules=['AS1F555'],
-    ext_modules=cythonize('AS1F555.py'),
-)
